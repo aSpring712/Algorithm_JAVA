68faced9af34636e09257aa419ce463b3ee3f03b
aSpring <rnjsqhal2001@naver.com>
2023-11-10 06:56:47 +0900

    대소문자 변환

---
+++ convertChar.java
@@ -0,0 +1,17 @@
+public class convertChar {
+    public String solution(String str) {
+        String answer = "";
+        for(char x : str.toCharArray()) {
+            if( x >= 97 && x <= 122 ) answer += (char)(x-32);
+            else answer += (char)(x+32);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        convertChar T = new convertChar();
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        System.out.print(T.solution(str));
+    }
+}
